bbb7f183e13e9706015dd835e236492663568bb1
Rustam Yunusov <yrustt@yandex.ru>
2017-12-11 02:11:06 +0300

    -

--- Lab_4/calc.c
+++ Lab_4/calc.c
@@ -8,29 +8,37 @@
 #include "models.h"
 
 
-int calc_hash_from_file(const char *path, struct Hash *hash) {
-    int bytes;
-    FILE *in = fopen(path, "rb");
+void calc_hash(FILE *file, struct Hash *hash) {
     MD5_CTX mdContext;
     unsigned char data[1024];
 
-    if (in == NULL) {
-        printf ("%s can't be opened.\n", path);
-        return -1;
-    }
-
     MD5_Init(&mdContext);
-    while ((bytes = fread (data, 1, 1024, in)) != 0) {
+    int bytes;
+    while ((bytes = fread(data, 1, 1024, file)) != 0) {
         MD5_Update(&mdContext, data, bytes);
     }
     MD5_Final(hash->value, &mdContext);
+}
 
-    fclose (in);
 
+int calc_hash_from_file(const char *path, struct Hash *hash) {
+    int bytes;
+    FILE *in = fopen(path, "rb");
+
+    if (in == NULL) {
+        printf ("%s can't be opened.\n", path);
+        return 1;
+    }
+
+    calc_hash(in, hash);
+
+    fclose(in);
     return 0;
 }
 
 
 int calc_hash_from_console(struct Hash *hash) {
+    calc_hash(stdin, hash);
+
     return 0;
 }
